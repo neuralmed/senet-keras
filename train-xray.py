4775e170e8b1bfbc58cbc98c477107654dd40e6a
nandaw <nandaw@gmail.com>
2019-09-18 18:44:03 -0300

    Comenta trechos referentes ao horovod

--- train-xray.py
+++ train-xray.py
@@ -1,11 +1,12 @@
 import keras
+from keras import optimizers
 from keras.utils import np_utils
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 from keras.callbacks import CSVLogger, ModelCheckpoint
 from keras.preprocessing.image import load_img, img_to_array
-import horovod.tensorflow as hvd
+# import horovod.tensorflow as hvd
 import keras.backend as K
 from keras.backend import tensorflow_backend
 import pandas as pd
@@ -16,44 +17,44 @@ import os
 import json
 # import configparser
 
-hvd.init()
+# hvd.init()
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = '0,1,2,3'
 
-class LearningRateScheduler(keras.callbacks.Callback):
-    """Learning rate scheduler.
-    # Arguments
-    schedule: a function that takes an epoch index as input
-    (integer, indexed from 0) and current learning rate
-    and returns a new learning rate as output (float).
-    verbose: int. 0: quiet, 1: update messages.
-    """
-    def __init__(self, schedule, verbose=0):
-        super(LearningRateScheduler, self).__init__()
-        self.schedule = schedule
-        self.verbose = verbose
-
-    def on_epoch_begin(self, epoch, logs=None):
-        print(self.model.optimizer)
-        if not hasattr(self.model.optimizer, 'learning_rate'):
-            raise ValueError('Optimizer must have a "learning_rate" attribute.')
-        learning_rate = float(K.get_value(self.model.optimizer.learning_rate))
-        try:  # new API
-            learning_rate = self.schedule(epoch, learning_rate)
-        except TypeError:  # old API for backward compatibility
-            learning_rate = self.schedule(epoch)
-        if not isinstance(learning_rate, (float, np.float32, np.float64)):
-            raise ValueError('The output of the "schedule" function '
-                             'should be float.')
-        K.set_value(self.model.optimizer.learning_rate, learning_rate)
-        if self.verbose > 0:
-            print('\nEpoch %05d: LearningRateScheduler setting learning '
-                  'rate to %s.' % (epoch + 1, learning_rate))
-
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        logs['lr'] = K.get_value(self.model.optimizer.learning_rate)
+# class LearningRateScheduler(keras.callbacks.Callback):
+#     """Learning rate scheduler.
+#     # Arguments
+#     schedule: a function that takes an epoch index as input
+#     (integer, indexed from 0) and current learning rate
+#     and returns a new learning rate as output (float).
+#     verbose: int. 0: quiet, 1: update messages.
+#     """
+#     def __init__(self, schedule, verbose=0):
+#         super(LearningRateScheduler, self).__init__()
+#         self.schedule = schedule
+#         self.verbose = verbose
+#
+#     def on_epoch_begin(self, epoch, logs=None):
+#         print(self.model.optimizer)
+#         if not hasattr(self.model.optimizer, 'learning_rate'):
+#             raise ValueError('Optimizer must have a "learning_rate" attribute.')
+#         learning_rate = float(K.get_value(self.model.optimizer.learning_rate))
+#         try:  # new API
+#             learning_rate = self.schedule(epoch, learning_rate)
+#         except TypeError:  # old API for backward compatibility
+#             learning_rate = self.schedule(epoch)
+#         if not isinstance(learning_rate, (float, np.float32, np.float64)):
+#             raise ValueError('The output of the "schedule" function '
+#                              'should be float.')
+#         K.set_value(self.model.optimizer.learning_rate, learning_rate)
+#         if self.verbose > 0:
+#             print('\nEpoch %05d: LearningRateScheduler setting learning '
+#                   'rate to %s.' % (epoch + 1, learning_rate))
+#
+#     def on_epoch_end(self, epoch, logs=None):
+#         logs = logs or {}
+#         logs['lr'] = K.get_value(self.model.optimizer.learning_rate)
 
 def normalize(im):
     im[:, :, 0] = (im[:, :, 0] - 103.94)
@@ -123,6 +124,8 @@ learning_rate = 0.1
 momentum = 0.9
 opt = tf.train.MomentumOptimizer(
             learning_rate=learning_rate, momentum=momentum, use_nesterov=True)
+sgd = optimizers.SGD(lr=learning_rate, momentum=momentum, nesterov=True)
+
 
 def lr_scheduler(epoch):
     print(epoch)
@@ -132,7 +135,8 @@ def lr_scheduler(epoch):
     return K.eval(model.optimizer.learning_rate)
 change_lr = LearningRateScheduler(lr_scheduler)
 model.compile(
-    optimizer= hvd.DistributedOptimizer(opt)
+    # optimizer= hvd.DistributedOptimizer(opt)
+    optimizer= sgd
     , loss='categorical_crossentropy'
     , metrics=['accuracy'])
 
